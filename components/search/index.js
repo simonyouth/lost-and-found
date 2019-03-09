321e1fcc6c6287423640dfa8b5a0bbfa36b43fb1
zyx <2515240522@qq.com>
2019-03-09 22:51:35 +0800

    add custom navigation bar

---
+++ components/search/index.js
@@ -0,0 +1,27 @@
+// components/search/index.js
+Component({
+  /**
+   * 外部样式类
+   */
+  externalClasses: ['out-search'],
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
