9545aa628588edf3575577b0db8adfe6a5741571
zyx <2515240522@qq.com>
2019-03-24 12:56:44 +0800

    componentized location with icon

---
+++ components/location-with-icon/location-with-icon.js
@@ -0,0 +1,24 @@
+// components/location-with-icon/location-with-icon.js
+Component({
+  externalClasses: ['offset'],
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    location: String,
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
