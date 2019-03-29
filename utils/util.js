4dbacd5c858bf0eb00d451b22c9161b6334ae872
simonyouth <2515240522@qq.com>
2019-03-29 18:52:00 +0800

    encapsulate wx.request to Promise

--- utils/util.js
+++ utils/util.js
@@ -3,3 +3,18 @@ import QQMapWX from './qqmap-wx-jssdk.min.js';
 export const qqmap = new QQMapWX({
   key: 'Q7MBZ-6LXWJ-FBQFV-KBKBX-WW4N2-OJBEM'
 });
+
+// 封装wx.request成Promise
+export const httpRequest = function(option) {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      ...option,
+      success: function (res) { 
+        resolve(res);
+      },
+      fail: function (res) {
+        reject(res);
+      },
+    })
+  })
+}
