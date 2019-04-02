f7e38b34fa25297144b6904be787f8f69ea06f94
simonyouth <2515240522@qq.com>
2019-04-02 15:37:39 +0800

    poi and region change in map

--- pages/map/index.js
+++ pages/map/index.js
@@ -1,39 +1,107 @@
 import { qqmap } from '../../utils/util.js';
+import cloneDeep from 'lodash.clonedeep';
+const test = new Array(20);
 Page({
   data: {
-    longitude: 113.3241,
-    latitude: 23.423,
+    longitude: Number,
+    latitude: Number,
+    markers: [{
+      iconPath: './images/center.png',
+      id: 1,
+      latitude: Number,
+      longitude: Number,
+      width: 40,
+      height: 40,
+    }, {
+      iconPath: './images/poi.png',
+      id: 2,
+      latitude: Number,
+      longitude: Number,
+      width: 40,
+      height: 40,
+    }],
+    suggestions: test.fill({name: '河南1111'}),
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * 生命周期函数--监听页面加载, 接受页面跳转传递的参数
    */
   onLoad: function (options) {
-   
+   wx.setNavigationBarTitle({
+     title: '定位查找'
+   });
+   // 设置标题栏style
+    wx.setNavigationBarColor({
+      backgroundColor: '#fff',
+      frontColor: '#000000'
+    });
   },
 
   onReady: function (e) {
     this.mapCtx = wx.createMapContext('myMap');
   },
   getCenterLocation: function () {
+    const markers = cloneDeep(this.data.markers);
     this.mapCtx.getCenterLocation({
       success: (res) => {
+        markers[0].latitude = res.latitude;
+        markers[0].longitude = res.longitude;
         this.setData({
-          latitude: res.latitude,
-          longitude: res.longitude,
+          markers,
         })
-        console.log(res.longitude)
-        console.log(res.latitude)
       }
     })
+  },
+  searchByKeywords: (word) => {
+    qqmap.search({
+      keyword: word,
+      success: (res) => {
+        console.log(res)
+      },
+    })
+  },
+  touchMap: function(res){
+    console.log(res)
+  },
+  handleRegion: function(e) {
+    if (e.type === 'end' && (e.causedBy === 'drag' || e.causedBy === 'scale')) {
+      this.getCenterLocation();
+    }
+  },
+  poitap: function(res) {
+    console.log(res);
+    const { latitude, longitude } = res.detail;
+    const markers = cloneDeep(this.data.markers);
+    markers[1].longitude = longitude;
+    markers[1].latitude = latitude;
+    this.setData({
+      markers,
+      suggestions: [res],
+    })
+  },
+  // 当前位置
+  getCurrentLocation: () => {
+    wx.getLocation({
+      type: 'gcj02',
+      success: res => {
+        this.setData({
+          longitude: res.longitude,
+          latitude: res.latitude,
+        })
+      }
+    });
+  },
+  // 移到中心点
+  moveToLocation: () => {
+    this.mapCtx.moveToLocation();
   },
   /**
    * 监听页面显示
    */
   onShow: function () {
     wx.getLocation({
+      type: 'gcj02',
       success: (res) => {
-        console.log(res)
         this.setData({
           longitude: res.longitude,
           latitude: res.latitude,
